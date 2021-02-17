95348d6a396f5670fd3aa2531452021f3047d779
Elliot <hello@elliotmorris.dev>
2021-02-17 19:40:33 +0000

    add validator for create budget

--- src/Outlay.Domain/Command/Budget/Create/CreateBudgetValidator.cs
+++ src/Outlay.Domain/Command/Budget/Create/CreateBudgetValidator.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿// <copyright file="CreateBudgetValidator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 namespace Outlay.Domain.Command.Budget.Create
 {
-    public class CreateBudgetValidator
+    using Chest.Core.Command;
+    using FluentValidation;
+
+    public class CreateBudgetValidator : CommandValidator<CreateBudget>
     {
-        public CreateBudgetValidator()
+        /// <inheritdoc/>
+        public override void ConfigureRules()
         {
+            this.RuleFor(x => x.UserId)
+               .NotEmpty()
+               .WithMessage("A valid user identifier must be provided");
+
+            this.RuleFor(x => x.Items)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("A budget items must be provided");
         }
     }
 }
